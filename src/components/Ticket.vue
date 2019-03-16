b332b98779565d88b8f5f6199ddac260a064515b
TAY TS <taytzushieh@gmail.com>
2019-03-16 09:06:27 +0800

    feature: Add Ticket.vue components to be injected inside the TicketListing.vue

---
+++ src/components/Ticket.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="ticket-holder" @click="openMessage">
+    <div class="ticket-title">{{title}}</div>
+    <div class="created">{{created}}</div>
+    <div class="lastActivity">{{lastActivity}}</div>
+    <div class="status">{{status}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ticket",
+  props: ["title", "created", "lastActivity", "status"]
+};
+</script>
+
+<style scoped>
+.ticket-holder {
+  width: 100%;
+  height: 80px;
+  margin: 10px 0;
+  border-radius: 5px;
+  padding: 5px 10px;
+  background-color: transparent;
+  border: solid 2px #9f2edf;
+}
+
+.ticket-holder:hover {
+  background-color: #908e91;
+  cursor: pointer;
+}
+
+.ticket-holder > * {
+  display: inline-block;
+  width: 15%;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.ticket-title {
+  width: 50%;
+}
+</style>
+
