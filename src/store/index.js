79f8fedc06b79f58d4a7eaa96a8b5f1caf638e92
TAY TS <taytzushieh@gmail.com>
2019-03-16 09:08:13 +0800

    feature: Add controller for listing tickets

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { user } from "./user/user";
+import { user } from "./user/user.js";
+import { tickets } from "./tickets/tickets.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    user
+    user,
+    tickets
   }
 });
