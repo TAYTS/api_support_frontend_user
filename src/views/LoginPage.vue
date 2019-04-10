ebfe6e96a0eae0e484e19772338be4c31a4fdbc9
Kenneth Sim <kensim28@hotmail.com>
2019-04-10 20:25:15 +0800

    feature: Updated emailRules js for login

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -93,7 +93,7 @@ export default {
       email: "",
       emailRules: [
         v => !!v || "Email is required",
-        v => /.+@.+\..+/.test(v) || "Email must be valid"
+        v => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || "Email must be valid"
       ],
       password: "",
       passwordRules: [v => !!v || "Password is required"],
